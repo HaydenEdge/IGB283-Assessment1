ae791b46ed956969660c30882e0cd4f95053a950
Hayden Edge <Hayden_edge@hotmail.com>
2017-08-23 09:43:13 +1000

    Added move function Also renamed my folder locally, let me know if that broke anything!

---
+++ Assets/Scripts/IGB283Transform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IGB283Transform : MonoBehaviour {
+
+	public float speed = 5.0f;
+	public bool moveX = true;
+	public bool moveY = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		Move (); //Just remove this line if you need
+	}
+
+
+	// Move the object (translation)
+	void Move() {
+		Vector3 position = this.transform.position;
+
+		// Change x-axis position
+		if (moveX == true) {
+			position.x += Time.deltaTime * speed;
+		}
+
+		// Change y-axis position
+		if (moveY == true) {
+			position.y += Time.deltaTime * speed;
+		}
+
+		this.transform.position = position;
+	}
+
+	// Rotation
+
+	// Scaling
+
+}
