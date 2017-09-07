5ab5392c025787e1b36fff649f66ecb06ef5b343
DESKTOP-PIUANDT\Cody <ccohdayn@gmail.com>
2017-09-07 13:06:50 +1000

    no message

--- Assets/Scripts/Triangle.cs
+++ Assets/Scripts/Triangle.cs
@@ -4,45 +4,38 @@ using UnityEngine;
 
 public class Triangle : MonoBehaviour {
 
-
     public Material material;
-	// Use this for initialization
-	void Start () {
-        // get and store Mesh functions on variable for easier use
+    private Mesh mesh;
+    // Use this for initialization
+    void Start () {
         gameObject.AddComponent<MeshFilter>();
         gameObject.AddComponent<MeshRenderer>();
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        mesh = GetComponent<MeshFilter>().mesh;
+
         GetComponent<MeshRenderer>().material = material;
 
         mesh.Clear();
 
-        mesh.vertices = new Vector3[]
-        {
-            new Vector3(0, 0, 0),
-            new Vector3(0, 1, 0),
-            new Vector3(1, 1, 0)
+        mesh.vertices = new Vector3[] {
+            new Vector3 (0, 0, 0),
+            new Vector3 (0, 1, 0),
+            new Vector3 (1, 1, 0),
+
         };
 
-        mesh.colors = new Color[]
-        {
-            new Color(0.8f, 0.3f, 1.0f),
-            new Color(0.8f, 1.0f, 1.0f),
-            new Color(0.0f, 0.3f, 1.0f)
+        mesh.colors = new Color[] {
+            new Color (0.8f, 0.3f, 1.0f),
+            new Color (0.8f, 0.3f, 1.0f),
+            new Color (0.8f, 0.3f, 1.0f),
+
         };
 
         mesh.triangles = new int[] { 0, 1, 2 };
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
-    public Vector3 IGB283Transform
-    {
-        get {
-            Vector3 newTriangle = new Vector3();
-            return newTriangle;
-        }
-    }
 }
